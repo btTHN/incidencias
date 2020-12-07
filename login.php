ee3f3afc000a1efba7e6234e29cb6220a6fc41be
Gordo <gordo@iMac-de-Josselyn.local>
2020-12-07 16:56:34 +0100

    primera version de login.php

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style>
+    .card-header {
+        text-align: center;
+        background-color: lightskyblue;
+    }
+
+    .container {
+        width: 20%;
+        margin: auto;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card tarjetas">
+            <div class="card-header">
+                <svg width="10em" height="10em" viewBox="0 0 16 16" class="bi bi-person-badge-fill" fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z" />
+                </svg>
+            </div>
+            <div class="card-body">Hola</div>
+        </div>
+    </div>
+</body>
+
+</html>
