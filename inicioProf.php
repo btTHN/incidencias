19133d6cc4d50b00d8fb24f9d1c4f2bd110584db
Gordo <gordo@iMac-de-Josselyn.local>
2020-12-08 02:02:22 +0100

    login final e inicio profe

---
+++ inicioProf.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+        integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="d-flex" id="content-wrapper">
+        <div class="menu-lateral">
+            <div class="title">
+                <h5>Gestion de incidencias</h5>
+            </div>
+            <div id="items">
+                <div id="item1" class="mb-3">
+                    <a href="#" class="active"><i class="fas fa-home"></i>Inicio</a>
+                </div>
+                <div id="item2" class="mb-3">
+                    <a href="#"><i class="fas fa-folder-open"></i>Mis incidencias</a>
+                </div>
+                <div id="item3" class="mb-3">
+                    <a href="#"><i class="fas fa-plus"></i>Crear incidencias</a>
+                </div>
+            </div>
+        </div>
+        <div id="content" class="bg-grey w-100">
+            <div id="barra-superior">
+                <div class="btitle w-50">
+                    <h1>Inicio</h1>
+                </div>
+                <div class="icons w-50">
+                    <div class="btn-toolbar" role="toolbar">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default"><span><i
+                                        class="fas fa-cog"></i></span></button>
+                            <button type="button" class="btn btn-default"><i class="fas fa-sign-out-alt"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
