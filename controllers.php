f2d29136be8c12eba997c433dc5bf0e03f9bd5c8
bdt <tipan88@gmail.com>
2021-01-03 18:15:20 +0100

    nada

--- controllers.php
+++ controllers.php
@@ -10,7 +10,8 @@ function validar_user()
             $datos = usuario::login_user($_POST['user'], $_POST['pswd']);
             if ($datos != null) {
                 $_SESSION['id_us'] = $datos['id'];
-                inicioProf();
+                require './head.php';
+                require './inicioProf.php';
             } else {
                 $_SESSION['error'] = true;
                 index();
@@ -20,20 +21,18 @@ function validar_user()
             index();
         }
     } else {
-        // require './head.php';
-        // require './inicioProf.php';
-        inicioProf();
+        require './head.php';
+        require './inicioProf.php';
     }
 }
 function inicioProf()
 {
     $datosInc = usuario::leerIncProf();
-    $tablaProf='<table <table id="example" class="table table-striped table-bordered table-hover" style="width: 100%">';
-    $tablaProf.='<thead><tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr></thead>';    
-    print_r($datosInc);
-    // if ($datosInc != null) {        
-    //     foreach($datosInc as $row){
-    //         echo($row['comentario']);
-    //     }       
-    // }
+    $tablaProf = '<table <table id="example" class="table table-striped table-bordered table-hover" style="width: 100%">';
+    $tablaProf .= '<thead><tr><th>Name</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th><th>Salary</th></tr></thead>';
+    if ($datosInc != null) {
+        foreach ($datosInc as $row) {
+            echo ($row['comentario']);
+        }
+    }
 }
--- inicioProf.php
+++ inicioProf.php
@@ -2,7 +2,7 @@
     <div class="d-flex" id="content-wrapper">
         <div class="menu-lateral">
             <div class="title">
-                <h5>Gestion de incidencias</h5>
+                <img src="../img/logo.png" width="250" alt="logo">
             </div>
             <div id="items">
                 <div id="item1" class="mb-3">
@@ -24,13 +24,53 @@
                 <div class="icons w-50">
                     <div class="btn-toolbar" role="toolbar">
                         <div class="btn-group">
-                            <a href=""><span><i class="fas fa-cog"></i></span></button></a>
+                            <a href=""><span><i class="fas fa-cog"></i></span></a>
                             <a href=""><i class="fas fa-sign-out-alt"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            
+            <br>
+            <div class="container h-100">
+                <div class="row flex-nowrap">
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card bg-success" style="max-width: 15rem;">
+                            <div class="card-header bg-behance content-center text-center">
+                                <h1>4</h1>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <h4>Resueltas</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card bg-warning" style="max-width: 15rem;">
+                            <div class="card-header bg-dribbble content-center text-center">
+                                <h1>5</h1>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <h4>Proceso</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card bg-danger" style="max-width: 15rem;">
+                            <div class="card-header content-center text-center">
+                                <h1>0</h1>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <h4>Pendientes</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
